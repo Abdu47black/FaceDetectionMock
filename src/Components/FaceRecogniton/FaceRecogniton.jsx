930ff424d47cfc4c67f991014b165bda6dcf65f5
abduredwan90@gmail.com <smart@kali.kali>
2024-01-30 16:19:46 +0300

    hola

---
+++ src/Components/FaceRecogniton/FaceRecogniton.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const FaceRecogntion = () => {
+	return (
+		<div className="center">
+			<img src={"https://samples.clarifai.com/metro-north.jpg"} alt="" />
+		</div>
+	);
+};
+export default FaceRecogntion;
